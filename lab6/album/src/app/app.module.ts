357314052cac9f0c16c770433ed89b600cb5e594
ballgimn <meytopintheworld@gmail.com>
2025-03-02 23:34:01 +0500

    .

---
+++ lab6/album/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { AboutComponent } from "./components/about/about.component";
+import { AlbumsComponent } from "./components/albums/albums.component";
+import { AlbumDetailComponent } from "./components/album-detail/album-detail.component";
+import { AlbumPhotosComponent } from "./components/album-photos/album-photos.component";
+
+const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch:'full'},
+    {path: 'home', component:HomeComponent},
+    {path: 'about', component:AboutComponent},
+    {path: 'albums', component:AlbumsComponent},
+    {path: 'albums/:id', component:AlbumDetailComponent },
+    {path: 'albums/:id/photos', component:AlbumPhotosComponent}
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
---
+++ lab6/album/src/app/components/albums/albums.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-albums',
+  imports: [],
+  templateUrl: './albums.component.html',
+  styleUrl: './albums.component.css'
+})
+export class AlbumsComponent {
+
+}
